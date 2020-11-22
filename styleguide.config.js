eb80632ac98bbf41e7e2753c86d6868124055e1d
jiang <643546122@qq.com>
2020-11-22 22:42:08 +0800

    fix: 修复编译报错

--- styleguide.config.js
+++ styleguide.config.js
@@ -6,9 +6,13 @@ module.exports = {
   title: 'Arctic业务组件库',
   version: packageFile.version, // 同上 使用 package.json 的 version
   usageMode: 'expand', // 自动打开文档的缩放
-  pagePerSection: true, // 是否每页一个组件显示
+  pagePerSection: process.env.NODE_ENV === 'production', // 是否每页一个组件显示
   styleguideDir: 'dist_docs', // 打包的目录
   components: 'src/components/**/*.tsx', // 写入对应目录的文档
+  defaultExample: true,
+  moduleAliases: {
+    'rsg-example': path.resolve(__dirname, 'src'),
+  },
   exampleMode: 'expand', // 表示示例代码是否展开或者合上文档中代码示例的标签初始化状态，决定是否展开
   webpackConfig: require('./webpack.config'),
   propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json').parse, // 用来支持 tsx
@@ -21,7 +25,7 @@ module.exports = {
       asyncAwait: false,
     },
   },
-  updateDocs(docs, file) {
+  updateDocs (docs, file) {
     if (docs.doclets.version) {
       const version = packageFile.version;
       docs.doclets.version = version;
