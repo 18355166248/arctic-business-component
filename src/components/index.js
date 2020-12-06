72b2d69c153bfe63e288b2bf4136a79995f13649
jianglang <643546122@qq.com>
2020-12-06 22:22:16 +0800

    feat: 添加打包文件

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 // -- APPEND ITEMS HERE --
 export { default as BaseModal } from './BaseModal';
 export { default as EditTable } from './EditTable';
+export { default as BaseButton } from './BaseButton';
